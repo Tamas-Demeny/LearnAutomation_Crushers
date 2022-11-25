b6cc076e8f5678a4267e4bc7291e684be4f91b49
Tamas Demeny <Tamas_Demeny@epam.com>
2022-11-25 10:37:06 +0200

    Packages for EPAM.com

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import java.sql.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] numeros = {};
+
+        System.out.println( solution(numeros));
     }
+
+    public static int solution(int[] numbers){
+        int i = 1;
+        numbers = Arrays.stream(numbers)
+                            .sorted()
+                            .distinct()
+                            .filter(x -> x > 0)
+                            .toArray();
+        for ( int j : numbers ) {
+                if ( i != j ) {
+                    return i;
+                } i++;
+        }
+        return i;
+    }
+
 }
