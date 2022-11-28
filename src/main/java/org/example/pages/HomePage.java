1f322238c824b8dc718e5c2a3bbb064e47bb0998
Andrei Stoean <andrei_stoean@epam.com>
2022-11-28 17:31:43 +0200

    Automated clicking on language DropDown Menu, clicking on english and clicking on hungary

---
+++ src/main/java/org/example/pages/HomePage.java
@@ -0,0 +1,43 @@
+package org.example.pages;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.example.BasePage;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class HomePage extends BasePage {
+
+    SelenideElement dropDownMenu = $(By.xpath("//div[@class=\"location-selector-ui header__control\"]/button"));
+    SelenideElement locationSelectorTitle = $(By.xpath("//strong[@class=\"location-selector__title\"]"));
+    SelenideElement globalEnglishLink = $(By.xpath("//a[@class=\"location-selector__link active\"]"));
+    SelenideElement hungaryLink = $(By.xpath("//li[@class=\"location-selector__item\"][2]/a"));
+
+    public HomePage clickLanguageDropDown() {
+        dropDownMenu.click();
+        return this;
+    }
+
+    public HomePage checkLanguageDropDownVisible() {
+        locationSelectorTitle.shouldBe(Condition.visible);
+        return this;
+    }
+
+    public HomePage checkLanguageDropDownNotVisible() {
+        locationSelectorTitle.shouldBe(Condition.hidden);
+        return this;
+    }
+
+    public HomePage clickGlobalEnglish() {
+        globalEnglishLink.click();
+        return this;
+    }
+
+    public HungaryHomePage clickHungary() {
+        hungaryLink.click();
+        return new HungaryHomePage();
+    }
+
+
+}
--- src/test/java/epamcomautomation/languagechange/LanguageChangeTests.java
+++ src/test/java/epamcomautomation/languagechange/LanguageChangeTests.java
@@ -1,6 +1,40 @@
 package epamcomautomation.languagechange;
 
 import basetests.EpamBaseTest;
+import com.codeborne.selenide.WebDriverRunner;
+import org.example.pages.HomePage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class LanguageChangeTests extends EpamBaseTest {
+
+    @Test
+    public void dropDownMenuAppears() {
+        var dropDownMenu = new HomePage()
+                .clickLanguageDropDown()
+                .checkLanguageDropDownVisible();
+
+    }
+
+    @Test
+    public void selectEnglishLanguage(){
+        var englishLanguage = new HomePage()
+                .clickLanguageDropDown()
+                .clickGlobalEnglish()
+                .checkLanguageDropDownNotVisible();
+
+        String currentUrl = WebDriverRunner.getWebDriver().getCurrentUrl();
+        Assert.assertEquals("https://www.epam.com/", currentUrl);
+    }
+
+    @Test
+    public void selectHungaryLanguage(){
+        var hungaryLanguage = new HomePage()
+                .clickLanguageDropDown()
+                .clickHungary();
+
+        String currentUrl = WebDriverRunner.getWebDriver().getCurrentUrl();
+        Assert.assertEquals("https://careers.epam.hu/", currentUrl);
+
+    }
 }
