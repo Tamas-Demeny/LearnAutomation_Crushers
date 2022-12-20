0e3866ff761843881cea29fbcc4f14a41945310d
Theodora Tataru <Theodora_Tataru@epam.com>
2022-12-20 15:43:47 +0200

    implemented search tests

--- src/test/java/basetests/BaseTest.java
+++ src/test/java/basetests/BaseTest.java
@@ -1,24 +1,29 @@
 package basetests;
 
 import com.codeborne.selenide.WebDriverRunner;
+import org.example.pages.SearchPage;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Selenide.*;
+import static org.example.utils.TypesOfData.LEARN_PAGE;
 
 
 public class BaseTest {
 
-    @BeforeMethod @Test
+    @BeforeMethod
     public void openStartPage() {
-        String startPage = "https://learn.epam.com/start";
-
-        open(startPage);
+        open(LEARN_PAGE);
 
         String url = WebDriverRunner.url();
 
-        Assert.assertEquals(startPage, url,
-                "We did not get to the expected page.");
+        Assert.assertEquals(LEARN_PAGE, url, "We did not get to the expected page.");
+    }
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        clearBrowserCookies();
+        closeWebDriver();
     }
 }
---
+++ src/test/java/searchtests/SearchTest.java
@@ -0,0 +1,66 @@
+package searchtests;
+
+import basetests.BaseTest;
+import org.example.pages.SearchPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.example.utils.TypesOfData.INVALID_INPUT;
+import static org.example.utils.TypesOfData.VALID_INPUT;
+
+public class SearchTest extends BaseTest {
+    SearchPage searchPage = new SearchPage();
+
+    //first
+    @Test
+    public void openCatalogPageTest() {
+        Assert.assertTrue( searchPage.clickSearch().checkCurrentUrl(), "The catalog page is not displayed !");
+    }
+
+    //second
+    @Test
+    public void searchValidInputTest() {
+        boolean checkValidInput = searchPage
+                .clickSearch()
+                .enterSearchInput(VALID_INPUT)
+                .checkAtLeastOneResultFound();
+        Assert.assertTrue(checkValidInput, "There is no course available according to your search!");
+    }
+
+    //third
+    @Test
+    public void searchInvalidInputTest() {
+        boolean checkInvalidInput = searchPage
+                .clickSearch()
+                .enterSearchInput(INVALID_INPUT)
+                .checkErrorMessageIsDisplayed();
+        Assert.assertTrue(checkInvalidInput, "The application displays wrong courses !");
+    }
+
+    //fourth
+    @Test
+    public void counterTest() {
+        boolean checkCounter = searchPage
+                .acceptCookies()
+                .clickSearch()
+                .enterSearchInput(VALID_INPUT)
+                .scrollToFooter(2)
+                .checkIfResultsCounterMatches();
+        Assert.assertTrue( checkCounter,"The counter doesn't work as expected !");
+    }
+
+    //fifth
+    @Test
+    public void clearButtonTest() {
+        boolean checkClearingResultsReturnsAllItems = searchPage
+                .acceptCookies()
+                .clickSearch()
+                .enterSearchInput(INVALID_INPUT)
+                .clearSearch()
+                .scrollToFooter(6)
+                .checkCounterMatchesResultsListSize();
+
+        Assert.assertTrue(checkClearingResultsReturnsAllItems, "The counter doesn't work as expected or not all available courses are displayed!");
+    }
+
+}
